4d4902d7e60e120f1561305a5b1d3fa7ad6215c0
Lindiel857 <56710478+Lindiel857@users.noreply.github.com>
2021-02-11 22:01:18 +0700

    Create eight.cpp

---
+++ eight.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    for (int u = 0; u < a; u++) {
+        int t;
+        cin >> t;
+        int j = 0;
+        int i = 1;
+        while (i <= t) {
+            i = i + j;
+            j = i - j;
+            if (i == t) {
+                cout << t << ' ';
+            }
+        }
+    }
+}
